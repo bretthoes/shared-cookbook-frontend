22f73ffec77884b02b1f3d7f7fcfc27132f1cd86
Brett Hoes <bretthoes@gmail.com>
2024-05-09 00:38:44 -0400

    add first run of recipe store

---
+++ lib/presentation/recipe/store/recipe_store.dart
@@ -0,0 +1,138 @@
+import 'package:boilerplate/core/extensions/string_extension.dart';
+import 'package:boilerplate/core/stores/error/error_store.dart';
+import 'package:boilerplate/domain/entity/recipe/recipe_list.dart';
+import 'package:boilerplate/domain/usecase/recipe/add_recipe_usecase.dart';
+import 'package:boilerplate/domain/usecase/recipe/get_recipe_usecase.dart';
+import 'package:boilerplate/utils/dio/dio_error_util.dart';
+import 'package:mobx/mobx.dart';
+
+part 'recipe_store.g.dart';
+
+class RecipeStore = _RecipeStore with _$RecipeStore;
+
+abstract class _RecipeStore with Store {
+  // constructor:---------------------------------------------------------------
+  _RecipeStore(
+    this._getRecipeUseCase,
+    this._addRecipeUseCase,
+    this.errorStore,
+    this.recipeErrorStore,
+  );
+
+  // use cases:-----------------------------------------------------------------
+  final GetRecipeUseCase _getRecipeUseCase;
+  final AddRecipeUseCase _addRecipeUseCase;
+
+  // stores:--------------------------------------------------------------------
+  // store for handling errors
+  final ErrorStore errorStore;
+  final RecipeErrorStore recipeErrorStore;
+
+  // store variables:-----------------------------------------------------------
+  static ObservableFuture<RecipeList?> emptyRecipeResponse =
+      ObservableFuture.value(null);
+
+  @observable
+  ObservableFuture<RecipeList?> fetchRecipesFuture =
+      ObservableFuture<RecipeList?>(emptyRecipeResponse);
+
+  @observable
+  RecipeList recipeList = new RecipeList(recipes: new List.empty());
+
+  @observable
+  String newCover = '';
+
+  @computed
+  bool get loading => fetchRecipesFuture.status == FutureStatus.pending;
+
+  // actions:-------------------------------------------------------------------
+  @action
+  Future getRecipes(int cookbookId) async {
+    final future = _getRecipeUseCase.call(params: cookbookId);
+    fetchRecipesFuture = ObservableFuture(future);
+
+    await future.then((recipeList) {
+      this.recipeList = recipeList;
+    }).catchError((error) {
+      errorStore.errorMessage = DioErrorUtil.handleError(error);
+    });
+  }
+
+  @action
+  Future addRecipe(
+    int creatorPersonId,
+    String title,
+    String cover,
+  ) async {
+    recipeErrorStore.error = validateAddRecipe(
+      creatorPersonId,
+      title,
+      cover,
+    );
+    if (recipeErrorStore.error.isNotEmpty) {
+      return;
+    }
+
+    final params = AddRecipeParams(
+      creatorPersonId: creatorPersonId,
+      title: title,
+      imagePath: cover,
+    );
+    final future = _addRecipeUseCase.call(params: params);
+
+    await future.then((addedRecipe) {
+      if (addedRecipe == null) {
+        throw Exception("Failed to add recipe: returned recipe is null");
+      }
+      recipeList.recipes.add(addedRecipe);
+      getRecipes(creatorPersonId);
+    }).catchError((error) {
+      errorStore.errorMessage = DioErrorUtil.handleError(error);
+    });
+  }
+
+  @action
+  String validateAddRecipe(int creatorPersonId, String title, String cover) {
+    var error = "";
+
+    if (creatorPersonId <= 0) {
+      error = "please sign in first";
+      return error;
+    }
+    if (title.isNullOrWhitespace) {
+      error = "please add a title";
+      return error;
+    }
+    if (cover.isNullOrWhitespace) {
+      error = "please add a cover image";
+      return error;
+    }
+    if (!RegExp(r'^[a-zA-Z0-9]+$').hasMatch(title)) {
+      error = "only alphanumeric characters allowed";
+      return error;
+    }
+    if (title.length < 2 || title.length > 18) {
+      error = "must be 2-18 chars";
+      return error;
+    }
+
+    return error;
+  }
+
+  @action
+  void setCover(String value) {
+    newCover = value;
+  }
+}
+
+class RecipeErrorStore = _RecipeErrorStore with _$RecipeErrorStore;
+
+abstract class _RecipeErrorStore with Store {
+  @observable
+  String error = '';
+
+  @action
+  void resetError() {
+    error = '';
+  }
+}
---
+++ lib/presentation/recipe/store/recipe_store.g.dart
@@ -0,0 +1,157 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'recipe_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$RecipeStore on _RecipeStore, Store {
+  Computed<bool>? _$loadingComputed;
+
+  @override
+  bool get loading => (_$loadingComputed ??=
+          Computed<bool>(() => super.loading, name: '_RecipeStore.loading'))
+      .value;
+
+  late final _$fetchRecipesFutureAtom =
+      Atom(name: '_RecipeStore.fetchRecipesFuture', context: context);
+
+  @override
+  ObservableFuture<RecipeList?> get fetchRecipesFuture {
+    _$fetchRecipesFutureAtom.reportRead();
+    return super.fetchRecipesFuture;
+  }
+
+  @override
+  set fetchRecipesFuture(ObservableFuture<RecipeList?> value) {
+    _$fetchRecipesFutureAtom.reportWrite(value, super.fetchRecipesFuture, () {
+      super.fetchRecipesFuture = value;
+    });
+  }
+
+  late final _$recipeListAtom =
+      Atom(name: '_RecipeStore.recipeList', context: context);
+
+  @override
+  RecipeList get recipeList {
+    _$recipeListAtom.reportRead();
+    return super.recipeList;
+  }
+
+  @override
+  set recipeList(RecipeList value) {
+    _$recipeListAtom.reportWrite(value, super.recipeList, () {
+      super.recipeList = value;
+    });
+  }
+
+  late final _$newCoverAtom =
+      Atom(name: '_RecipeStore.newCover', context: context);
+
+  @override
+  String get newCover {
+    _$newCoverAtom.reportRead();
+    return super.newCover;
+  }
+
+  @override
+  set newCover(String value) {
+    _$newCoverAtom.reportWrite(value, super.newCover, () {
+      super.newCover = value;
+    });
+  }
+
+  late final _$getRecipesAsyncAction =
+      AsyncAction('_RecipeStore.getRecipes', context: context);
+
+  @override
+  Future<dynamic> getRecipes(int cookbookId) {
+    return _$getRecipesAsyncAction.run(() => super.getRecipes(cookbookId));
+  }
+
+  late final _$addRecipeAsyncAction =
+      AsyncAction('_RecipeStore.addRecipe', context: context);
+
+  @override
+  Future<dynamic> addRecipe(int creatorPersonId, String title, String cover) {
+    return _$addRecipeAsyncAction
+        .run(() => super.addRecipe(creatorPersonId, title, cover));
+  }
+
+  late final _$_RecipeStoreActionController =
+      ActionController(name: '_RecipeStore', context: context);
+
+  @override
+  String validateAddRecipe(int creatorPersonId, String title, String cover) {
+    final _$actionInfo = _$_RecipeStoreActionController.startAction(
+        name: '_RecipeStore.validateAddRecipe');
+    try {
+      return super.validateAddRecipe(creatorPersonId, title, cover);
+    } finally {
+      _$_RecipeStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setCover(String value) {
+    final _$actionInfo = _$_RecipeStoreActionController.startAction(
+        name: '_RecipeStore.setCover');
+    try {
+      return super.setCover(value);
+    } finally {
+      _$_RecipeStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+fetchRecipesFuture: ${fetchRecipesFuture},
+recipeList: ${recipeList},
+newCover: ${newCover},
+loading: ${loading}
+    ''';
+  }
+}
+
+mixin _$RecipeErrorStore on _RecipeErrorStore, Store {
+  late final _$errorAtom =
+      Atom(name: '_RecipeErrorStore.error', context: context);
+
+  @override
+  String get error {
+    _$errorAtom.reportRead();
+    return super.error;
+  }
+
+  @override
+  set error(String value) {
+    _$errorAtom.reportWrite(value, super.error, () {
+      super.error = value;
+    });
+  }
+
+  late final _$_RecipeErrorStoreActionController =
+      ActionController(name: '_RecipeErrorStore', context: context);
+
+  @override
+  void resetError() {
+    final _$actionInfo = _$_RecipeErrorStoreActionController.startAction(
+        name: '_RecipeErrorStore.resetError');
+    try {
+      return super.resetError();
+    } finally {
+      _$_RecipeErrorStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+error: ${error}
+    ''';
+  }
+}
