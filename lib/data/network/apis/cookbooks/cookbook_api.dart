6ee81426ba70ad84a5144729b649ba7c070439d8
brett <bretthoes@gmail.com>
2024-07-03 22:59:07 -0400

    update add cookbook call to send a file

--- lib/data/network/apis/cookbooks/cookbook_api.dart
+++ lib/data/network/apis/cookbooks/cookbook_api.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:boilerplate/core/data/network/dio/dio_client.dart';
 import 'package:boilerplate/data/network/constants/endpoints.dart';
 import 'package:boilerplate/domain/entity/cookbook/cookbook.dart';
 import 'package:boilerplate/domain/entity/cookbook/cookbook_list.dart';
 import 'package:boilerplate/domain/usecase/cookbook/add_cookbook_usecase.dart';
+import 'package:dio/dio.dart';
 
 class CookbookApi {
   // dio instance
@@ -31,10 +33,22 @@ class CookbookApi {
         title: params.title,
         imagePath: params.imagePath,
       );
+
+      var coverFile = File(cookbook.imagePath!);
+
+      // Create a FormData object
+      var formData = FormData.fromMap({
+        'creatorPersonId': cookbook.creatorPersonId,
+        'title': cookbook.title,
+        'cover': await MultipartFile.fromFile(coverFile.path,
+            filename: coverFile.path.split('/').last),
+      });
+
       final res = await _dioClient.dio.post(
         Endpoints.addCookbook,
-        data: cookbook.toMap(),
+        data: formData,
       );
+
       var newCookbook = Cookbook.fromMap(res.data);
       return newCookbook;
     } catch (e) {
